dffd4e776a9541c8b42f29a8897d80664eabae90
LR <1204326279@qq.com>
2017-07-05 10:19:46 +0800

    首次提交

---
+++ 函数/函数/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 函数
+{
+
+    class Program
+    {
+        static int Add(int var1,int var2)
+        {
+            int sum = var1 + var2;
+            return sum;
+        }
+
+        static float Add(float var1, float var2)
+        {
+            float sum = var1 + var2;
+            return sum;
+        }
+
+        struct Location
+        {
+            public string country;
+            public string province;
+
+            public string show()
+            {
+                return country + " " + province;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Location myLocation;
+            myLocation.country = "中国";
+            myLocation.province = "湖北";
+            Console.WriteLine(myLocation.show());
+            Console.ReadKey();
+        }
+    }
+}
