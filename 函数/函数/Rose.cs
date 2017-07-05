c583b828113da215dd3ec0fde4817a1dd9140574
LR <1204326279@qq.com>
2017-07-05 10:46:39 +0800

    Rose add Class:Rose

---
+++ 函数/函数/Rose.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 函数
+{
+    class Rose
+    {
+        public void Print()
+        {
+            Console.WriteLine("我是Rose");
+        }
+    }
+}
